67ad82baa60d59844af2926409dc079207d6a331
lwc_gl <mai2533atlwc@aliyun.com>
2018-10-01 12:28:26 +0800

    Test Big Apple

--- src/main/java/com/atlwc/git/apple.java
+++ src/main/java/com/atlwc/git/apple.java
@@ -2,6 +2,6 @@ package com.atlwc.git;
 
 public class apple {
 	public static void main(String[] args) {
-		System.out.println("This is apple....");
+		System.out.println("This is big apple....");
 	}
 }
